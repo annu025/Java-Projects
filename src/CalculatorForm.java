d56569aaa6ab01db271167ff7a934057a1d26240
annu025 <anundhara.c@gmail.com>
2019-02-18 20:53:02 -0800

    Adding JButtons to the CalculatorForm

---
+++ src/CalculatorForm.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculatorForm extends JFrame implements ActionListener, MouseListener{
+    double value1, value2, result;
+    char op;
+    JLabel label;
+    JButton button0;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JButton button5;
+    JButton button6;
+    JButton button7;
+    JButton button8;
+    JButton button9;
+    JButton buttonDot;
+    JButton buttonEq;
+    JButton buttonA;
+    JButton buttonS;
+    JButton buttonD;
+    JButton buttonM;
+    JButton buttonBS;
+    JButton buttonCL;
+
+    JPanel panel;
+
+    CalculateForm(){
+
+    }
+
+
+
+
+}
